8be663490f8501a419655d5af2ad4cae74c605e6
nandakishore-k <nandakishorek2002@gmail.com>
2024-03-31 16:15:56 +0530

    file upload in case list

--- src/app/singlecase/singlecase.component.ts
+++ src/app/singlecase/singlecase.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from "../units/navbar/navbar.component";
+import { FileuploadComponent } from '../dataTransfer/fileupload/fileupload.component';
 
 @Component({
     selector: 'app-singlecase',
     standalone: true,
     templateUrl: './singlecase.component.html',
     styleUrl: './singlecase.component.css',
-    imports: [RouterLink, NavbarComponent]
+    imports: [RouterLink, NavbarComponent,FileuploadComponent]
 })
 export class SinglecaseComponent {
 
