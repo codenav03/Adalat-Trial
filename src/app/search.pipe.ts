38a6190356da4d6705842718e67089ffae8a4d96
nandakishore-k <nandakishorek2002@gmail.com>
2024-04-05 09:57:42 +0530

    search2

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -6,8 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, args?: any): any {
+    if(!value) return null;
+    if(!args)return value;
+
+    args=args.toLowerCase();
+    return value.filter((item: any)=>{
+      return JSON.stringify(item).toLowerCase().includes(args);
+    })
+
   }
 
 }
