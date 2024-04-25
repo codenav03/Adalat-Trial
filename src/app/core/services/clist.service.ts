52f20a35f0ddb1b2e2236248f3c2516a2aabe104
nandakishore-k <nandakishorek2002@gmail.com>
2024-04-25 22:12:46 +0530

    bug fix

--- src/app/core/services/clist.service.ts
+++ src/app/core/services/clist.service.ts
@@ -32,7 +32,7 @@ updateCase(key: string, expense: Icasel){
 }
 
 assgCourt(key: string,lcourtId: string,date: string){
-  this.caseRef.update(key,{ assign: 'YES' ,lcourtId: lcourtId,date: date,flag:"NOREAD"});
+  this.caseRef.update(key,{ assign: 'YES' ,lcourtId: lcourtId,date: date,flag:"NOTSEEN"});
 }
 //need to add flag notseen
 
---
+++ src/app/lower-new-cases/lower-new-cases.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lower-new-cases',
+  standalone: true,
+  imports: [],
+  templateUrl: './lower-new-cases.component.html',
+  styleUrl: './lower-new-cases.component.css'
+})
+export class LowerNewCasesComponent {
+
+}
